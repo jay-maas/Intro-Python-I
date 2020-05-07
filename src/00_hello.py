4ea38831fb7ff0948026090d8348f6096513aa0e
Jay Maas <jaymaas.dev@gmail.com>
2020-05-07 11:51:03 -0600

    Hello, Bignum, Datatypes, Modules
    Completed the above files MVP

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,2 +1,2 @@
 # Print "Hello, world!" to your terminal
-print('Hellow, world!')
+print('Hello, world!')
--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,40 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print('command line arguements: ', sys.argv)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+platValue = sys.platform
+
+if platValue == 'linux':
+    platStr = 'Linux'
+elif platValue == 'win32':
+    platStr = 'Windows'
+elif platValue == 'cygwin':
+    platStr = 'Windows/Cygwin'
+elif platValue == 'darwin':
+    platStr = 'Max OS X'
+
+print('Current OS platform: ', platStr)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+pyVer = sys.version_info
+pyVerStr = f'v{pyVer.major}.{pyVer.minor}.{pyVer.micro}'
+print('Current Python Version: ', pyVerStr)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print('current proccess ID: ', os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print('current working directory: ', os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print('current machine login name: ', os.getlogin())
