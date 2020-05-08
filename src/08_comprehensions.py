0352e7a0cf95e3f054b97387ab7280b79d49a61f
Jay Maas <jaymaas.dev@gmail.com>
2020-05-07 20:23:07 -0600

    comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -1,34 +1,43 @@
-"""
-List comprehensions are one cool and unique feature of Python.
-They essentially act as a terse and concise way of initializing
-and populating a list given some expression that specifies how
-the list should be populated. 
+# """
+# List comprehensions are one cool and unique feature of Python.
+# They essentially act as a terse and concise way of initializing
+# and populating a list given some expression that specifies how
+# the list should be populated. 
 
-Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
-for more info regarding list comprehensions.
-"""
+# Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+# for more info regarding list comprehensions.
+# """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+# y = [i for i in range(1,6)]
+# for number in range(6):
+#     if number != 0:
+#         y.append(number)
 
-print (y)
+# print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+# y = [i**3 for i in range(10)]
+# for number in range(10):
+#     y.append(number**3)
 
-print(y)
+
+# print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
-a = ["foo", "bar", "baz"]
+# a = ["foo", "bar", "baz"]
 
-y = []
+# y = [i.upper() for i in a]
 
-print(y)
+# for string in a:
+#     y.append(string.upper())
+
+# print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
@@ -36,6 +45,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [int(i) for i in x if int(i) % 2 == 0]
 
 print(y)
